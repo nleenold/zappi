9187786530102f18a7beee69aa701fbc6ffd03c5
Leenold T Ndalama <nleenold@yahoo.com>
2021-03-24 19:38:43 +0200

    Add files via upload

---
+++ question 1 discussion/zappi.ps1
@@ -0,0 +1,45 @@
+## VM Account
+# Credentials for Local Admin account you created in the sysprepped (generalized) vhd image
+$VMLocalAdminUser = "LocalAdminUser"
+$VMLocalAdminSecurePassword = ConvertTo-SecureString "Password" -AsPlainText -Force
+## Azure Account
+$LocationName = "South Africa North"
+$ResourceGroupName = "MyResourceGroup"
+# This a Premium_LRS storage account.
+# It is required in order to run a client VM with efficiency and high performance.
+$StorageAccount = "Mydisk"
+
+## VM
+$OSDiskName = "MyOSDisk"
+$ComputerName = "ZAPPI_TEST"
+$OSDiskUri = "https://Mydisk.blob.core.windows.net/disks/MyOSDisk.vhd"
+$SourceImageUri = "https://Mydisk.blob.core.windows.net/vhds/MyOSImage.vhd"
+$VMName = "TEST_VM"
+# Modern hardware environment with fast disk, high IOPs performance.
+# Required to run a client VM with efficiency and performance
+$VMSize = "Standard_DS3"
+$OSDiskCaching = "ReadWrite"
+$OSCreateOption = "FromImage"
+
+## Networking
+$DNSNameLabel = "mydnsname" # mydnsname.southafricanorth.cloudapp.azure.com
+$NetworkName = "ZAPINET"
+$NICName = "MyNIC"
+$PublicIPAddressName = "MyPIP"
+$SubnetName = "ZAPISUB"
+$SubnetAddressPrefix = "10.0.0.0/24"
+$VnetAddressPrefix = "10.0.0.0/16"
+
+$SingleSubnet = New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
+$Vnet = New-AzVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
+$PIP = New-AzPublicIpAddress -Name $PublicIPAddressName -DomainNameLabel $DNSNameLabel -ResourceGroupName $ResourceGroupName -Location $LocationName -AllocationMethod Dynamic
+$NIC = New-AzNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
+
+$Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
+
+$VirtualMachine = New-AzVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Linux -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
+$VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -SourceImageUri $SourceImageUri -Caching $OSDiskCaching -CreateOption $OSCreateOption -Linux
+
+New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $VirtualMachine -Verbose
